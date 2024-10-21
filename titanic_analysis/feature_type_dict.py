45da098164974c0e101cd32f52e47e4571fbfabb
syj24psx <syj24psx@bangor.ac.uk>
2024-10-21 02:47:14 +0100

    Update feature_type_dict.py

--- titanic_analysis/feature_type_dict.py
+++ titanic_analysis/feature_type_dict.py
@@ -24,18 +24,15 @@ def create_feature_type_dict(df: pd.DataFrame) -> dict:
     # Iterate through DataFrame columns
     for column in df.columns:
         if pd.api.types.is_numeric_dtype(df[column]):
-            # Classify features based on unique value counts and domain knowledge
             if column in ['Age', 'Fare'] or len(df[column].unique()) > 20:  # Treat Age and Fare as continuous
                 feature_types['numerical']['continuous'].append(column)
-            else:  # Heuristic for discrete
+            else:
                 feature_types['numerical']['discrete'].append(column)
-        elif pd.api.types.is_categorical_dtype(df[column]) or isinstance(df[column].dtype, pd.CategoricalDtype):
-            feature_types['categorical']['nominal'].append(column)
-        elif df[column].dtype == 'object':
+        elif pd.api.types.is_categorical_dtype(df[column]) or df[column].dtype == 'object':
             feature_types['categorical']['nominal'].append(column)
 
-    # Example of adding ordinal features based on domain knowledge
-    ordinal_features = ['Pclass']  # Add any other ordinal features you know
+    # Add ordinal features based on domain knowledge
+    ordinal_features = ['Pclass']
     feature_types['categorical']['ordinal'].extend(ordinal_features)
 
     return feature_types
