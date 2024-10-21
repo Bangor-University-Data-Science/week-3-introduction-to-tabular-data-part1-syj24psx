737588c9c4f698dc937c03a7aab945c64bc601fc
syj24psx <syj24psx@bangor.ac.uk>
2024-10-21 01:55:04 +0100

    Update data_loader.py

--- titanic_analysis/data_loader.py
+++ titanic_analysis/data_loader.py
@@ -11,10 +11,10 @@ def load_titanic_data(filepath: str) -> pd.DataFrame:
         pd.DataFrame: Loaded Titanic dataset as a DataFrame.
     """
     try:
-        df = pd.read_csv("DS-ICE-4006/titanic.csv")
+        df = pd.read_csv(filepath)
         return df
     except FileNotFoundError:
-        print(f"Error: The file at {"DS-ICE-4006/titanic.csv"} was not found.")
+        print(f"Error: The file at {filepath} was not found.")
         return pd.DataFrame()  # Return an empty DataFrame if the file is not found
     except pd.errors.EmptyDataError:
         print("Error: The file is empty.")
@@ -22,9 +22,3 @@ def load_titanic_data(filepath: str) -> pd.DataFrame:
     except Exception as e:
         print(f"An error occurred: {e}")
         return pd.DataFrame()  # Return an empty DataFrame for any other exceptions
-    pass
-
-# Example usage:
-# df = load_titanic_data('path/to/titanic.csv')
-# print(df.head())
-
