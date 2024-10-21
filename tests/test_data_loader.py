2fa66b9576895e54662dfaa54570fbe372776e00
syj24psx <syj24psx@bangor.ac.uk>
2024-10-21 03:12:22 +0100

    Update test_data_loader.py

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -3,7 +3,7 @@ from titanic_analysis.data_loader import load_titanic_data
 
 def test_load_titanic_data_valid():
     # Use an absolute path to the Titanic CSV file
-    df = load_titanic_data("D:/DataScience_W3/week-3-introduction-to-tabular-data-part1-syj24psx/data/titanic.csv")
+    df = load_titanic_data("data/titanic.csv")
     
     # Check that the returned object is a DataFrame
     assert isinstance(df, pd.DataFrame), "The returned object should be a DataFrame"
